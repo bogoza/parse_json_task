19b78c754ef1a712a0e6b433e44b3b587558715a
Giorgi Chachanidze <72442468+bogoza@users.noreply.github.com>
2023-12-14 22:01:29 +0400

    Base Fragment created

---
+++ app/src/main/java/com/example/jsonparsetask/data_model/Input.kt
@@ -0,0 +1,14 @@
+package com.example.jsonparsetask.data_model
+
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Input(
+    val field_id: Int,
+    val hint: String,
+    val field_type: String,
+    val keyboard: String,
+    val required: Boolean,
+    val is_active: Boolean,
+    val icon: String
+)
